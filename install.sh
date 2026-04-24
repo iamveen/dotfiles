338aa830f819aacbe45dc86de38baecbadea2d1d
Gavin Dunne <g@veen.ca>
2026-04-24 01:09:32 +0000

    Use script directory for stow path in install script

--- install.sh
+++ install.sh
@@ -1,11 +1,13 @@
 #!/usr/bin/env sh
 set -e
 
+SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
+
 sudo apt-get -y update
 sudo apt-get -y upgrade
 sudo apt-get -y install build-essential unzip stow tree entr pkg-config libssl-dev fish stow
 sudo chsh -s $(which fish) $USER
 
 curl https://mise.run | sh
-$(cd $HOME/dotfiles/stow && stow --target "$HOME" *)
+$(cd "$SCRIPT_DIR/stow" && stow --target "$HOME" *)
 $HOME/.local/bin/mise install
